7ad2bef763f557375bf02bd7983f4702ef79cf58
Aashish Panthee <aashishpanthee060@gmail.com>
2025-05-03 16:10:16 +0545

    (chore): setup database connection - create User model - add API's ( for signup, userProfile, allUsers)

--- src/app.js
+++ src/app.js
@@ -1,14 +1,58 @@
-const express = require('express')
+const express = require("express");
+const databaseConnection = require("./config/database");
+const { User } = require("./models/user");
 
-const app = express()
+const app = express();
 
-app.use('/test', (req, res) => {
-  res.send('Hi from testpage')
-})
-app.use('/', (req, res) => {
-  res.send('Hi from homepage')
-})
+// parse the body of the request from json to javascript object (middleware)
+app.use(express.json());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
-})
+app.post("/signup", async (req, res) => {
+  const userObj = req.body;
+
+  // create a new instance of the User model
+  const user = new User(userObj);
+
+  try {
+    await user.save();
+    res.status(201).send("User created successfully");
+  } catch (error) {
+    res.status(500).send("Error saving user to the database" + error.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(400).send("Something went wrong" + error.message);
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (!users) {
+      res.status(404).send("No users found");
+    }
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(400).send("Something went wrong" + error.message);
+  }
+});
+
+databaseConnection()
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(4000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.log("Database connection failed", error);
+  });
