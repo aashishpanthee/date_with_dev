08a962fe05433d413874d24e46b35e4f7b1c54b1
Aashish Panthee <aashishpanthee060@gmail.com>
2025-06-30 16:25:41 +0545

    feat: add update profileData and password functionality

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,23 +1,59 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData, validatePasswordChangeData } = require('../utils/validation');
 
+const saltRounds = 10;
 
 profileRouter.get("/view", userAuth, async (req, res) => {
   try {
     const { user } = req;
-    res.status(200).send(user);
+    res.status(200).json({
+      message: "Profile data fetched successfully",
+      data: user
+    });
   } catch (error) {
     res.status(500).send("ERROR. " + error.message);
   }
 })
 
 profileRouter.patch("/edit", userAuth, async (req, res) => {
-
+  try {
+    const isValidFields = validateEditProfileData(req.body);
+    if (!isValidFields) {
+      return res.status(400).send("Update not allowed. Please check the fields you are trying to update.");
+    }
+    const loggedInUser = req.user;
+    Object.keys(req.body).forEach((key) => {
+      loggedInUser[key] = req.body[key];
+    });
+    await loggedInUser.save();
+    res.status(200).json({
+      message: "Profile updated successfully",
+      data: loggedInUser
+    });
+  } catch (error) {
+    res.status(500).send("ERROR. " + error.message);
+  }
 })
 
 profileRouter.patch("/password", userAuth, async (req, res) => {
+  try {
+    const { password, newPassword } = req.body;
+    const loggedInUser = req.user;
 
+    await validatePasswordChangeData(password, newPassword, loggedInUser);
+    const hashedPassword = await bcrypt.hash(newPassword, saltRounds);
+    loggedInUser.password = hashedPassword;
+    await loggedInUser.save();
+    res.status(200).json({
+      message: "Password updated successfully",
+      data: loggedInUser
+    });
+  } catch (error) {
+    return res.status(500).send("ERROR. " + error.message);
+  }
 })
 
 module.exports = profileRouter;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,4 +1,5 @@
 const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 /**
  * The function `validateSignUpData` validates the sign-up data provided by a user to ensure all
@@ -38,4 +39,47 @@ const validateSignUpData = (userObj) => {
   }
 };
 
-module.exports = { validateSignUpData };
+/**
+ * The function `validateEditProfileData` checks if the fields in a user object are allowed for editing
+ * based on a predefined list of allowed fields.
+ * @param userObj - The `userObj` parameter in the `validateEditProfileData` function is an object that
+ * contains the fields and values that a user wants to update in their profile. 
+ * @returns The function `validateEditProfileData` returns a boolean value indicating whether all the
+ * fields in the `userObj` parameter are allowed for update based on the `allowedEditFields` array.
+ */
+const validateEditProfileData = (userObj) => {
+  const allowedEditFields = ["firstName", "lastName", "age", "gender", "password", "photoUrl", "about", "skills"];
+  const IS_ALLOWED_UPDATE = Object.keys(userObj).every((field) => allowedEditFields.includes(field));
+  return IS_ALLOWED_UPDATE;
+}
+
+/**
+ * The function `validatePasswordChangeData` ensures that the old and new passwords meet certain
+ * criteria for a logged-in user.
+ * @param oldPassword - The `oldPassword` parameter represents the current password of the logged-in
+ * user.
+ * @param newPassword - The `newPassword` parameter in the `validatePasswordChangeData` function
+ * represents the new password that the user wants to change to.
+ * @param loggedInUser - loggedInUser is an object representing the user who is currently logged in. It
+ * likely contains information such as the user's username, email, and methods to verify the user's
+ * password.
+ */
+const validatePasswordChangeData = async (oldPassword, newPassword, loggedInUser) => {
+  if (!oldPassword || !newPassword) {
+    throw new Error("Both old and new passwords are required");
+  }
+
+  const isOldPasswordValid = await loggedInUser.verifyPassword(oldPassword);
+  if (!isOldPasswordValid) {
+    throw new Error("Password is not valid");
+  }
+
+  if (oldPassword === newPassword) {
+    throw new Error("New password must be different from old password");
+  }
+  if (!validator.isStrongPassword(newPassword)) {
+    throw new Error("New password must be strong");
+  }
+};
+
+module.exports = { validateSignUpData, validateEditProfileData, validatePasswordChangeData };
