0ac6e916b039821b29ce53dd31a6b4eb506197ed
Aashish Panthee <aashishpanthee060@gmail.com>
2025-07-07 08:53:03 +0545

    add: cors package

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const databaseConnection = require("./config/database");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
@@ -8,6 +9,19 @@ const requestRouter = require("./routes/request");
 const userRouter = require('./routes/user');
 
 const app = express();
+
+/* `app.use(cors())` is setting up CORS (Cross-Origin Resource Sharing) middleware in the Express
+application. CORS is a security feature implemented by browsers to restrict web pages from making
+requests to a different domain than the one that served the original web page. By using `cors()`,
+you are allowing your server to handle requests from different origins, enabling cross-origin
+communication between your server and clients. */
+app.use(cors({
+  "origin": "http://localhost:5173",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204,
+  credentials: true
+}))
 
 // parse the body of the request from json to javascript object (middleware)
 app.use(express.json());
